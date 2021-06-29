78956e667eed8e145281cad7a686fc9860703658
runru1030 <runru1030@gmail.com>
2021-06-30 00:07:52 +0900

    FEAT : get user from /auth/kakao

--- src/component/App.js
+++ src/component/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import queryString from 'query-string';
 import { useDispatch, useSelector } from 'react-redux';
 import { setLoggedInfo, setSetting } from '../modules/user';
+import Footer from './Footer'
 function App() {
   const dispatch = useDispatch();
   const { userObj, isLoggedin, isSetting } = useSelector(state => ({
@@ -20,28 +21,23 @@ function App() {
       getKakaoTokenHandler(query);
     }
     else {
-      if (userObj!=null&&token&&(new Date().getTime()-token.received_at)>0) {
+      if (userObj != null && token && (new Date().getTime() - token.received_at) > 0) {
         //user setting정보 확인
-        if(userObj.nickName!=""){
+        if (userObj.nickName != "") {
           dispatch(setSetting(true));
         }
-        else{
+        else {
           dispatch(setSetting(false));
         }
       }
       else {
         dispatch(setLoggedInfo(null, false));
-        //slocalStorage.removeItem("token");
+        window.localStorage.removeItem("token");
       }
     }
 
   }, []);
-  
-  useEffect(() => {
-    //userID로 userObj 검색
-    /*userID로 DB검색후, 토큰 만료 시, 로그아웃*/
-    //getUserfromServer(token);
-  }, [userObj]);
+
   //카카오 rest api 토큰 발급
   const getKakaoTokenHandler = async (query) => {
     const data = {
@@ -61,22 +57,33 @@ function App() {
     }).then((res) => {
       //발급받은 액세스 토큰 저장
       window.localStorage.setItem("token", JSON.stringify({
-        access_token: res.data.access_token, 
+        access_token: res.data.access_token,
         refresh_token: res.data.refresh_token,
         refresh_token_expires_in: 5183999,
-        received_at:new Date().getTime()
+        received_at: new Date().getTime()
       }));
       //서버에 user정보 요청
       console.log(res.data);
-      getUserfromServer(res.data);
+      getUserfromServer({
+        access_token: res.data.access_token,
+        refresh_token: res.data.refresh_token
+      });
       window.history.replaceState({}, null, window.location.origin + window.location.hash);
     });
   }
   //토큰으로 서버에 user요청
   const getUserfromServer = (token) => {
-    axios.post('http://localhost:8001/auth/kakao', {...token })
+    axios.post('/auth/kakao', { ...token })
       .then(res => {
-        if (res.status == 200||res.status == 201) {
+        console.log(res.data);
+        if (res.status == 200) {
+          window.localStorage.setItem("token", JSON.stringify({
+            ...res.data.kakaoToken,
+            refresh_token_expires_in: 5183999,
+            received_at: new Date().getTime()
+          }));
+        }
+        if (res.status == 201 || res.status == 200) {
           //setting 검사
           const user=res.data.user;
           dispatch(setLoggedInfo(user, true));
@@ -94,7 +101,8 @@ function App() {
   }
   return (<>
     <AppRouter isLoggedin={isLoggedin} isSetting={isSetting} userObj={userObj} token={token} getUserfromServer={getUserfromServer} />
-    <footer><span>dobdob 2021 footer</span></footer></>
+    <Footer/>
+</>
   );
 }
 
