b055d3e3bd0be56673474247e46826fafc6b0bea
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-07-04 23:39:34 +0900

    Fix : PostImages slider

---
+++ src/component/SliderSlick.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Slider from "react-slick";
+const SliderSlick =({imgArr})=> {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      arrows:true,
+    }
+    return (
+      <div className="slider">
+        <Slider {...settings}>
+          {imgArr.map(img=> <div className="img-wrapper">
+            <img src={img.url}/></div>)}
+        </Slider>
+      </div>
+    );
+  }
+
+export default SliderSlick;
