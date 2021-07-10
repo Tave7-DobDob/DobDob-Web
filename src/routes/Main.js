fa8ea6820d6f26fcdc24cbcd3086dcd4489b045e
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-07-10 20:43:56 +0900

    Feat : API /post [post] & post 검색

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -2,13 +2,12 @@ import REACT, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import PostContainer from '../component/PostContainer';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt, faPlus, faSlidersH, faCaretDown, faUserCircle, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-import moment from 'moment';
+import { faMapMarkerAlt, faPlus, faSlidersH, faCaretDown, faUserCircle, faSignOutAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 import DaumPost from '../component/DaumPost';
 import Modal from '../component/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { setProfileInfo } from '../modules/profileInfo';
-import { setLoggedInfo } from '../modules/user';
+import { setLoggedInfo, setUserInfo } from '../modules/user';
 import './main.css';
 import axios from 'axios';
 const Main = () => {
@@ -16,51 +15,39 @@ const Main = () => {
     const dispatch = useDispatch()
     const userObj = useSelector(state => state.user.userObj);
     const [search, setSearch] = useState("");
-    const [searchPost, setSearchPost] = useState();
-    const [postList, setPostList] = useState(
-        [/* {
-            postId: 123,
-            userId: { userId: 1, nickName: "김지현", locationId: { dong: "상도동" } }
-            , locationId: { dong: "상도동" },
-            title: "같이 배드민턴 쳐요",
-            content: "공원에서 같이 배드민턴 칠 사람 구해요 :-)",
-            createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
-            tag: ["운동", "배드민턴"],
-            heart: 12,
-            comment: 2
-        }, {
-            postId: 1234,
-            userId: { userId: 2, nickName: "사용자", locationId: { dong: "노량진동" } }
-            , locationId: { dong: "상도동" },
-            title: "맛집좀 알려주세요!",
-            content: "동네 맛집좀 알려주세요!\n이사온지 얼마 안돼서 주변 식당을 잘모릅니다 :-(",
-            createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
-            tag: ["우리동네", "맛집", "추천"],
-            heart: 2,
-            comment: 10
-        } */]);//test데이터
+    const [postList, setPostList] = useState([]);
     const [isOpenDaum, setIsOpenDaum] = useState(false);
-    const [locationObj, setLocationObj] = useState(userObj.locationId);
+    const [locationObj, setLocationObj] = useState(userObj.Location);
     const [isOpenMoal, setIsOpenModal] = useState(false);
     useEffect(() => {
         //locationId을 통해서 포스트 리스트 받기
-        //setPostList();
-        axios.get("/post").then(res => {
-            setPostList([...res.data.posts])
+        axios.post("/post", {
+            locationX: parseFloat(locationObj.locationX),
+            locationY: parseFloat(locationObj.locationY)
+        }).then(res => {
+            setPostList(res.data.posts)
         });
-        
-    },[]);
+    }, []);
+    useEffect(() => {
+        //location변경 후, 포스트 리스트 받기
+        axios.post("/post", {
+            locationX: parseFloat(locationObj.locationX),
+            locationY: parseFloat(locationObj.locationY)
+        }).then(res => {
+            setPostList(res.data.posts)
+        });
+    }, [locationObj]);
 
     const onChange = (event) => {
         const { target: { value } } = event;
         setSearch(value);
-        if (value[0] == "#") {
-            console.log(value);
-            setSearchPost(postList.filter(post => post.tag.filter(it => it.includes(value.substr(1, value.length))).length != 0));
-        }
-        else {
-            setSearchPost(postList.filter(post => post.title.includes(value)));
-        }
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        search[0] == "#" ?
+            axios.get(`/post/list/tag/${search.substring(1)}`).then(res => setPostList(res.data.posts))//태그 검색
+            :
+            axios.get(`/post/list/${search}`).then(res => setPostList(res.data.posts)) //제목 검색
     }
 
     const onClickMyPage = () => {
@@ -71,7 +58,13 @@ const Main = () => {
         history.push("/posting");
     }
     const onClickLogo = () => {
-        history.push("/");
+        setSearch("");
+        axios.post("/post", {
+            locationX: parseFloat(locationObj.locationX),
+            locationY: parseFloat(locationObj.locationY)
+        }).then(res => {
+            setPostList(res.data.posts)
+        });
     }
     const onClickLogout = () => {
         dispatch(setLoggedInfo(null, false));
@@ -79,7 +72,6 @@ const Main = () => {
         window.location.replace("/")
     }
     const onClickLocation = () => {
-        //동네설정 modal
         setIsOpenDaum(prev => !prev);
     }
     const onModalClick = () => {
@@ -90,8 +82,12 @@ const Main = () => {
     }
     return (<div className="Container main">
         <header><img src="logo2.png" width="60px" onClick={onClickLogo} />
-            <input type="text" onChange={onChange} placeholder="태그 및 내용을 검색해보세요 !" value={search} />
-            <button id="tag-char" onClick={onClickshap}>#</button>
+            <form className="row-container" onSubmit={onSubmit}>
+                <input type="text" onChange={onChange} placeholder="제목 및 태그를 검색해보세요 !" value={search} />
+                <input type="submit" value="검색" id="search-btn" style={{ display: "none" }} />
+            </form>
+            <button id="tag-char-btn" onClick={onClickshap}>#</button>
+            <label id="search-btn-label" for="search-btn"><FontAwesomeIcon icon={faSearch} /></label>
             <div className="profile-wrapper">
                 <div onClick={onClickMyPage} className="profile-img" data-toggle="tooltip" title="마이 페이지"><img src={userObj.profileUrl ? userObj.profileUrl : "profile_img.png"} /></div>
                 <span>{userObj.nickName}</span>
@@ -111,12 +107,11 @@ const Main = () => {
                 <span>{locationObj ? locationObj.dong : "동네를 설정해주세요."}</span>
             </div>
             <div className="centerContainer post-wrapper">
-                {postList.length == 0 || (search != "#" && search != "" && searchPost.length == 0) ? <>
+                {postList.length == 0 ? <>
                     <div className="centerContainer nothing-post-wrapper"><p>해당 동네의 게시물이 아직 없습니다.<br />먼저 글을 작성해보세요 !</p>
                         <img src="nothingPost.png" width="60%" />
                     </div>
-                </> : <>{search == "" || search == "#" ? postList.map(post => <PostContainer postObj={post} userObj={userObj} />) :
-                    searchPost.map(post => <PostContainer postObj={post} userObj={userObj} />)}</>}
+                </> : postList.map(post => <PostContainer postObj={post} userObj={userObj} />)}
             </div>
             <FontAwesomeIcon id="plus-icon" icon={faPlus} data-toggle="tooltip" title="글쓰기" onClick={onClickPosting} />
         </div>
