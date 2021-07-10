a91860fd79e75dba3edbc59046ec5a6f6a0be70a
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-07-11 00:26:39 +0900

    Feat : API /comment

--- src/component/Comment.js
+++ src/component/Comment.js
@@ -1,20 +1,15 @@
+import axios from 'axios';
 import REACT, { useEffect, useState } from 'react';
 import MentionHighlight from './MentionHighlight';
 import ProfileBox from './ProfileBox';
 const Comment = ({ commentObj, isOwner }) => {
-    const [writer, setWriter] = useState({});
-    useEffect(() => {
-        //userId로 검색요청
-        setWriter(commentObj.userId);
-    }, [])
 
     const onDelClick = () => {
-        //isDeleted:true 로 변경요청
+        axios.delete(`/comment/${commentObj.id}`)
     }
     return (<div className="comment">
         <div className="post-profile-wrapper">
-            <ProfileBox profileObj={writer} locationId={writer.locationId} />
-            <span id="date">{commentObj.createdAt}</span>
+            <ProfileBox profileObj={commentObj.User} location={commentObj.User.Location} date={commentObj.createdAt}/>
             {isOwner && <button id="del-btn" onClick={onDelClick}>삭제</button>}
         </div>
         <div>
