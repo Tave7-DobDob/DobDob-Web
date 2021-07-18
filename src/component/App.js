58c1d1b7aea0f299d14607e7e9ff8063e20ea542
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-07-18 21:46:18 +0900

    Fix : jwt 헤더 추가 & 에러 해결

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,5 @@
 import AppRouter from './Router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import axios from 'axios';
 import queryString from 'query-string';
 import { useDispatch, useSelector } from 'react-redux';
@@ -14,20 +14,23 @@ function App() {
     isLoggedin: state.user.isLoggedin,
     isSetting: state.user.isSetting
   }))
-  const token = useState(JSON.parse(window.localStorage.getItem('token')));
+  const accessToken = JSON.parse(window.localStorage.getItem("token"));
+  if(accessToken){
+    axios.defaults.headers.common["Authorization"] = `${accessToken.access_token}`;
+  }
   const query = queryString.parse(window.location.search);
 
   useEffect(() => {
-    if (window.location.pathname == "/oauth") {
+    if (query.code) {
       getKakaoTokenHandler(query);
     }
     else {
-      if(userObj == null || !token ) {
+      if(userObj == null || !accessToken.access_token ) {
         dispatch(setLoggedInfo(null, false));
         window.localStorage.removeItem("token");
       }
       else {
-        sendJwtTokenToServer(token);
+        sendJwtTokenToServer();
       }
     }
 
@@ -37,32 +40,34 @@ function App() {
     const data = {
       grant_type: "authorization_code",
       client_id: process.env.REACT_APP_KAKAO_REST_KEY,
-      redirect_uri: "http://localhost:3000/oauth",
+      redirect_uri: "https://tave7-dobdob.github.io/DobDob-Web/",
       code: query.code
     };
     const queryString = Object.keys(data)
       .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(data[k]))
       .join('&');
-
     axios.post('https://kauth.kakao.com/oauth/token', queryString, {
       headers: {
         'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
       }
     }).then((res) => {
+      
       sendKakaoTokenToServer({
         access_token: res.data.access_token,
       });
-      window.history.replaceState({}, null, window.location.origin + window.location.hash);
+      window.history.replaceState({}, null, window.location.origin+ window.location.pathname + window.location.hash);
     });
   }
   //일반 로그인
   const sendKakaoTokenToServer = (token) => {
-    axios.post('/auth/kakao',{...token })
+    axios.post('http://ec2-3-34-137-99.ap-northeast-2.compute.amazonaws.com/auth/kakao',{...token })
       .then(res => {
         if (res.status == 201 || res.status == 200) {
           window.localStorage.setItem("token", JSON.stringify({
             access_token: res.data.jwt
-          }));
+          })); 
+          axios.defaults.headers.common["Authorization"] = `${res.data.jwt}`;
+
           const user=res.data.user;
           dispatch(setLoggedInfo(user, true));
           if (user.nickName != "") {
@@ -78,14 +83,10 @@ function App() {
       })
   }
   //자동로그인
-  const sendJwtTokenToServer = (jwtToken) => {
-    console.log("token");
-    axios.post('/auth/kakao',{
-      headers:{
-        'Authorization': jwtToken
-      }
-     })
+  const sendJwtTokenToServer = () => {
+    axios.post('http://ec2-3-34-137-99.ap-northeast-2.compute.amazonaws.com/auth/kakao')
       .then(res => {
+        
         if ( res.status == 200) {
           const user=res.data.user;
           dispatch(setLoggedInfo(user, true));
